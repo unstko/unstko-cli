39dc4bf8e31bca22f5b8416d2ab203dfc4bfa3d7
Stefan Koch <code@unstko.de>
2020-05-22 21:04:13 +0200

    Initial main application

---
+++ unstko.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from plumbum import cli
+
+class Unstko(cli.Application):
+    PROGNAME = "unstko CLI"
+    VERSION = "0.1"
+    DESCRIPTION = "The command line interface (CLI) with Applications from @unstko"
+
+    def main(self, *args):
+        if args:
+            print("Unknown command {0!r}".format(args[0]))
+            return 1
+        if not self.nested_command:
+            print("No command given")
+            return 1
+
+if __name__ == "__main__":
+    Unstko.run()
