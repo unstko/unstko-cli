6903d6fbd84cc9e64b44eab294f7e82468e24cab
Stefan Koch <code@unstko.de>
2020-05-31 00:31:57 +0200

    Add working main method to linux subcommand

--- linux/unstko_linux.py
+++ linux/unstko_linux.py
@@ -3,12 +3,17 @@
 from plumbum import cli
 
 class UnstkoLinux(cli.Application):
-    PROGNAME = "unstko_linux"
+    PROGNAME = "unstko linux"
     VERSION = "0.1"
     DESCRIPTION = "Linux commands"
 
-    def main(self):
-        print("Linux")
+    def main(self, *args):
+        if args:
+            print("Unknown command {0!r}".format(args[0]))
+            return 1
+        if not self.nested_command:
+            print("No command given")
+            return 1
 
 UnstkoLinux.subcommand("update-system", "update_system.UpdateSystem")
 
